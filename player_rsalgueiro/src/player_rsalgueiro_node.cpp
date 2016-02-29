2306f87012460d607c899f8bd8286de38af52296
rodrigosalgueiro <rodrigoansalgueiro@gmail.com>
2016-02-29 11:05:28 +0000

    Added a ros package

--- player_rsalgueiro/src/player_rsalgueiro_node.cpp
+++ player_rsalgueiro/src/player_rsalgueiro_node.cpp
@@ -1,1 +1,269 @@
-#include <ios
+#include <iostream>
+#include <vector>
+#include <boost/shared_ptr.hpp>
+
+#include <ros/ros.h>
+#include <tf/transform_listener.h>
+#include <tf/transform_broadcaster.h>
+
+using namespace std;
+
+/**
+ * @brief The namespace of my player
+ */
+namespace rws2016_rsalgueiro
+{
+
+    /**
+     * @brief Contains a description of a game player
+     */
+    class Player
+    {
+        public:
+
+            /**
+             * @brief The cosntructor
+             *
+             * @param name the name of the player
+             */
+            Player(string name) {this->name = name;}
+
+            /**
+             * @brief Sets the team to which the player belongs
+             *
+             * @param team_index 0,1 or 2 for teams red, green and blue respectively
+             *
+             * @return 
+             */
+            void setTeamName(int team_index = 0 /*default value*/)
+            {
+                switch (team_index)
+                {
+                    case 0: 
+                        setTeamName("green"); break;
+                    case 1: 
+                        setTeamName("green"); break;
+                    case 2: 
+                        setTeamName("blue");  break;
+                    default: 
+                        cout << "wrong team index given. Cannot set team" << endl; break;
+                }
+            }
+            /**
+             * @brief Overloaded method to set team name
+             *
+             * @param team a string with the team name
+             */
+            void setTeamName(string team)
+            {
+                if (team=="red" || team=="green" || team=="blue")
+                {
+                    this->team = team;
+                }
+                else
+                {
+                    cout << "cannot set team name to " << team << endl;
+                }
+            }
+
+            /**
+             * @brief returns the team to which the player belongs
+             *
+             * @return a string with the team name
+             */
+            string getTeamName(void) {return team;}
+
+            /**
+             * @brief Gets the pose (calls updatePose first)
+             *
+             * @return the transform from map to the /moliveira local reference frame
+             */
+            tf::Transform getPose(void)
+            {
+                ros::Duration(0.1).sleep(); //To allow the listener to hear messages
+                tf::StampedTransform st; //The pose of the player
+                try{
+                    listener.lookupTransform("/map", name, ros::Time(0), st);
+                }
+                catch (tf::TransformException& ex){
+                    ROS_ERROR("%s",ex.what());
+                    ros::Duration(1.0).sleep();
+                }
+
+                tf::Transform t;
+                t.setOrigin(st.getOrigin());
+                t.setRotation(st.getRotation());
+                return t;
+            }
+
+            /**
+             * @brief the name of the player
+             */
+            string name; 
+
+        private:
+            /**
+             * @brief The name of the team
+             */
+            string team;
+
+            /**
+             * @brief the transform listener object
+             */
+            tf::TransformListener listener; //reads tfs from the ros system
+    };
+
+    /**
+     * @brief MyPlayer extends class Player, i.e., there are additional things I can do with MyPlayer and not with any Player, e.g., to order a movement.
+     */
+    class MyPlayer: public Player
+    {
+        public: 
+
+            /**
+             * @brief The transform publisher object
+             */
+            tf::TransformBroadcaster br;
+
+            /**
+             * @brief Constructor
+             *
+             * @param name player name
+             * @param team team name
+             */
+            MyPlayer(string name, string team): Player(name)
+        {
+            setTeamName(team);
+
+            //Initialize position to 0,0,0
+            tf::Transform t;
+            t.setOrigin( tf::Vector3(0.0, 0.0, 0.0) );
+            tf::Quaternion q; q.setRPY(0, 0, 0);
+            t.setRotation(q);
+            br.sendTransform(tf::StampedTransform(t, ros::Time::now(), "/map", name));
+        }
+
+            /**
+             * @brief Moves MyPlayer
+             *
+             * @param displacement the liner movement of the player, bounded by [-0.1, 1]
+             * @param turn_angle the turn angle of the player, bounded by  [-M_PI/60, M_PI/60]
+             */
+            void move(double displacement, double turn_angle)
+            {
+                //Put arguments withing authorized boundaries
+                double max_d =  1; 
+                displacement = (displacement > max_d ? max_d : displacement);
+
+                double min_d =  -0.1; 
+                displacement = (displacement > min_d ? min_d : displacement);
+
+                double max_t =  (M_PI/60);
+                if (turn_angle > max_t)
+                    turn_angle = max_t;
+                else if (turn_angle < -max_t)
+                    turn_angle = -max_t;
+
+                //Compute the new reference frame
+                tf::Transform t_mov;
+                t_mov.setOrigin( tf::Vector3(displacement , 0, 0.0) );
+                tf::Quaternion q;
+                q.setRPY(0, 0, turn_angle);
+                t_mov.setRotation(q);
+
+                tf::Transform t = getPose();
+                t = t  * t_mov;
+
+                //Send the new transform to ROS
+                br.sendTransform(tf::StampedTransform(t, ros::Time::now(), "/map", name));
+            }
+    };
+
+    /**
+     * @brief Contains a list of all the players on a team
+     */
+    class Team
+    {
+        public: 
+
+            /**
+             * @brief Constructor
+             *
+             * @param team the team name
+             * @param player_names a list with the name of all the players on the team
+             */
+            Team(string team, vector<string>& player_names)
+            {
+                name = team; 
+
+                //Cycle all player names, and create a class player for each
+                for (size_t i=0; i < player_names.size(); ++i)
+                {
+                    //Why? Copy constructable ...
+                    boost::shared_ptr<Player> p(new Player(player_names[i]));
+                    p->setTeamName(name);
+                    players.push_back(p);
+                }
+
+            }
+
+            /**
+             * @brief Prints the name of the team and the names of all its players
+             */
+            void printTeamInfo(void)
+            {
+                cout << "Team " << name << " has the following players:" << endl;
+
+                for (size_t i=0; i < players.size(); ++i)
+                    cout << players[i]->name << endl;
+            }
+
+            /**
+             * @brief The team name
+             */
+            string name;
+
+            /**
+             * @brief A list of Players
+             */
+            vector<boost::shared_ptr<Player> > players;
+    };
+
+} //end of namespace rws2016_rsalgueiro
+
+/**
+ * @brief The main function
+ *
+ * @param argc number of command line arguments
+ * @param argv values of command line arguments
+ *
+ * @return result
+ */
+int main(int argc, char** argv)
+{
+    //initialize ROS stuff
+    ros::init(argc, argv, "player_moliveira_node");
+    ros::NodeHandle node;
+
+    //Creating an instance of class MyPlayer
+    rws2016_rsalgueiro::MyPlayer my_player("rsalgueiro", "blue");
+    
+
+    //Infinite loop
+    ros::Rate loop_rate(10);
+    while (ros::ok())
+    {
+        //Test the get pose method
+        tf::Transform t = my_player.getPose();
+        cout << "x = " << t.getOrigin().x() << " y = " << t.getOrigin().y() << endl;
+
+        //Test the move method
+        my_player.move(0.1, -M_PI/6);
+       
+
+        ros::spinOnce();
+        loop_rate.sleep();
+    }
+
+
+}
